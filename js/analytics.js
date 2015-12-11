d554a9cccfd474bb672d80c4871d83cf9422aa47
ssf.developer <ssf.developer@gmail.com>
2015-12-11 15:25:28 +0100

    Update statistics

--- js/analytics.js
+++ js/analytics.js
@@ -1,8 +1,6 @@
-(function($, ga) {
-    $(document).ready(function() {
-        $('a[data-type=download]').on('click', function(event) {
-            var element = $(this);
-            ga('send', 'event', 'Download', 'download', element.data('file'));
-        });
+jQuery(document).ready(function() {
+    $('a[data-type=download]').on('click', function(event) {
+        var element = $(this);
+        ga('send', 'event', 'Download', 'download', element.data('file'));
     });
-})(jQuery, ga);
+});
