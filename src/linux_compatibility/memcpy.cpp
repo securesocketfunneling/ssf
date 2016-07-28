fed0b9a951ddbcba80609b15f1dca191060ea142
ssf.developer <ssf.developer@gmail.com>
2016-07-28 21:30:07 +0200

    Freeze memcpy api version

---
+++ src/linux_compatibility/memcpy.cpp
@@ -0,0 +1,16 @@
+#if defined( __GNUC__ )  &&  defined( __LP64__ )  &&  __LP64__ >= 1  && \
+    (_GNUC__ >= 5  ||  (__GNUC__ == 4  &&  __GNUC_MINOR__ >= 7))  &&  \
+    (defined( __x86_64__ )  ||  defined( __i386__ )  ||\
+    defined( __i486__ )  ||  defined( __i586__ )  ||  defined( __i686__ ))
+
+#include <string.h>
+
+__asm__(".symver memcpy, memcpy@GLIBC_2.2.5");
+
+extern "C" {
+	void *__wrap_memcpy(void *dest, const void *src, size_t n) {
+		return memcpy(dest, src, n); 
+	}
+}  
+
+#endif
