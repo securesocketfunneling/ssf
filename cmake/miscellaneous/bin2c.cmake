64bd8aba53928dd6da87740caa38e2dd153760f6
olachere <olachere@protonmail.com>
2017-09-26 22:33:46 +0200

    certs: create inlineable C header files from tests certificates

---
+++ cmake/miscellaneous/bin2c.cmake
@@ -0,0 +1,8 @@
+function(bin2c varname input output)
+get_filename_component(output ${output} ABSOLUTE BASE_DIR ${CMAKE_CURRENT_BINARY_DIR})
+message("Generating C header file ${output} from binary file ${input}")
+file(READ ${input} _data HEX)
+string(REGEX MATCH "([^/]+)$" _filename ${input})
+string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," _data ${_data})
+file(WRITE ${output} "unsigned char ${varname}[] = { ${_data} };")
+endfunction(bin2c)
