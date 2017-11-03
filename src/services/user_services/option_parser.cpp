b3d9b2676cda3f052be1aea4aa070f57c8d07313
olachere <olachere@protonmail.com>
2017-11-03 21:53:19 +0100

    option-parser: Parse listening and forward options manually

--- src/services/user_services/option_parser.cpp
+++ src/services/user_services/option_parser.cpp
@@ -1,12 +1,3 @@
-#ifndef BOOST_SPIRIT_USE_PHOENIX_V3
-#define BOOST_SPIRIT_USE_PHOENIX_V3 1
-#endif
-#include <boost/fusion/include/std_pair.hpp>
-#include <boost/fusion/include/vector.hpp>
-#include <boost/optional.hpp>
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/classic.hpp>
-
 #include <ssf/log/log.h>
 
 #include "common/error/error.h"
@@ -22,118 +13,90 @@ ForwardOptions::ForwardOptions() : from(), to() {}
 
 ForwardOptions OptionParser::ParseForwardOptions(
     const std::string& option, boost::system::error_code& ec) {
-  using boost::spirit::qi::ushort_;
-  using boost::spirit::qi::alnum;
-  using boost::spirit::qi::char_;
-  using boost::spirit::qi::lit;
-  using boost::spirit::qi::rule;
-  typedef std::string::const_iterator str_it;
-
   ForwardOptions forward_options;
+  char *end;
+  unsigned long val;
+  std::string::size_type s, e;
 
-  rule<str_it, std::string()> local_addr_pattern = *char_("0-9a-zA-Z.-");
-  rule<str_it, std::string()> target_pattern = +char_("0-9a-zA-Z.-");
-
-  str_it begin = option.begin(), end = option.end();
-
-  // basic format
-  if (boost::spirit::qi::parse(
-          begin, end, ushort_ >> ":" >> target_pattern >> ":" >> ushort_,
-          forward_options.from.port, forward_options.to.addr,
-          forward_options.to.port) &&
-      begin == end) {
-    ec.assign(::error::success, ::error::get_ssf_category());
-    return forward_options;
+  s = option.rfind(':');
+  if (s == std::string::npos) {
+    ec.assign(::error::invalid_argument, ::error::get_ssf_category());
+    return {};
   }
 
-  forward_options = {};
-  begin = option.begin(), end = option.end();
-  // extended format
-  if (boost::spirit::qi::parse(
-          begin, end, local_addr_pattern >> ":" >> ushort_ >> ":" >>
-                          target_pattern >> ":" >> ushort_,
-          forward_options.from.addr, forward_options.from.port,
-          forward_options.to.addr, forward_options.to.port) &&
-      begin == end) {
-    if (forward_options.from.addr.empty()) {
-      // empty value means all network interfaces
-      forward_options.from.addr = "*";
-    }
-
-    // not found
-    ec.assign(::error::success, ::error::get_ssf_category());
-    return forward_options;
+  end = NULL;
+  val = std::strtoul(option.c_str() + s + 1, &end, 10);
+  if (*end != '\0' || val > 65535) {
+    ec.assign(::error::invalid_argument, ::error::get_ssf_category());
+    return {};
   }
+  forward_options.to.port = val;
 
-  ec.assign(::error::invalid_argument, ::error::get_ssf_category());
+  e = s;
+  s = option.rfind(':', e - 1);
+  if (s == std::string::npos) {
+    ec.assign(::error::invalid_argument, ::error::get_ssf_category());
+    return {};
+  }
+  forward_options.to.addr = std::string(option, s + 1, e - (s + 1));
+
+  e = s;
+  s = option.rfind(':', e - 1);
+
+  end = NULL;
+  if (s != std::string::npos) {
+    forward_options.from.addr = std::string(option, 0, s);
+    val = std::strtoul(option.c_str() + s + 1, &end, 10);
+  } else {
+    forward_options.from.addr = "";
+    val = std::strtoul(option.c_str(), &end, 10);
+  }
+  if (*end != ':' || val > 65535) {
+    ec.assign(::error::invalid_argument, ::error::get_ssf_category());
+    return {};
+  }
+  forward_options.from.port = val;
 
   return forward_options;
 }
 
 Endpoint OptionParser::ParseListeningOption(const std::string& option,
                                             boost::system::error_code& ec) {
-  using boost::spirit::qi::ushort_;
-  using boost::spirit::qi::alnum;
-  using boost::spirit::qi::char_;
-  using boost::spirit::qi::lit;
-  using boost::spirit::qi::rule;
-  typedef std::string::const_iterator str_it;
-
   Endpoint listening_option;
+  unsigned long val;
+  char *end = NULL;
+  std::string::size_type s = option.find(':');
 
-  rule<str_it, std::string()> local_addr_pattern = *char_("0-9a-zA-Z.-");
-
-  str_it begin = option.begin(), end = option.end();
-
-  // basic format
-  if (boost::spirit::qi::parse(begin, end, ushort_, listening_option.port) &&
-      begin == end) {
+  if (s != std::string::npos) {
+    listening_option.addr = std::string(option, 0, s);
+    val = std::strtoul(option.c_str() + s + 1, &end, 10);
+  } else {
     listening_option.addr = "";
-    ec.assign(::error::success, ::error::get_ssf_category());
-    return listening_option;
+    val = std::strtoul(option.c_str(), &end, 10);
   }
-
-  listening_option = {};
-  begin = option.begin(), end = option.end();
-  // extended format
-  if (boost::spirit::qi::parse(begin, end, local_addr_pattern >> ":" >> ushort_,
-                               listening_option.addr, listening_option.port) &&
-      begin == end) {
-    if (listening_option.addr.empty()) {
-      // empty value means all network interfaces
-      listening_option.addr = "*";
-    }
-    ec.assign(::error::success, ::error::get_ssf_category());
-    return listening_option;
+  if (*end != '\0' || val > 65535) {
+    ec.assign(::error::invalid_argument, ::error::get_ssf_category());
+    return {};
   }
-
-  // not found
-  ec.assign(::error::invalid_argument, ::error::get_ssf_category());
+  listening_option.port = val;
 
   return listening_option;
 }
 
 uint16_t OptionParser::ParsePort(const std::string& port,
                                  boost::system::error_code& ec) {
-  try {
-    std::size_t pos = 0;
-    uint32_t res = std::stoul(port, &pos);
-    if (pos != port.size()) {
-      ec.assign(::error::invalid_argument, ::error::get_ssf_category());
-      return 0;
-    }
-    if (res < 1 || res > 65535) {
-      SSF_LOG("parser", error, "invalid port: {} not in [1 - 65355]", port);
-      ec.assign(::error::invalid_argument, ::error::get_ssf_category());
-      return 0;
-    }
-    return res;
-  } catch (...) {
-    SSF_LOG("parser", error, "invalid port: {}", port);
+  unsigned long val;
+  char *end = NULL;
+
+  val = std::strtoul(port.c_str(), &end, 10);
+  if (*end != '\0' || val > 65535) {
     ec.assign(::error::invalid_argument, ::error::get_ssf_category());
     return 0;
   }
+
+  return val;
 }
+
 
 }  // services
 }  // ssf
